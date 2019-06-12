281bc16ee042018798afd77eeb0005c41095dff9
Jean-Francois Chevrette <jfchevrette@gmail.com>
2019-06-12 18:10:17 -0400

    update build script

--- hack/build_deploy.sh
+++ hack/build_deploy.sh
@@ -4,6 +4,10 @@ DOCKER_CONF="$PWD/.docker"
 mkdir -p "$DOCKER_CONF"
 docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 
-# build images
-make build image-push
+# Build the binary
+docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app golang:1.12 make build
+
+# Build image
+make image-push
+
 
