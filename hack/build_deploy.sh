29f64244eba0ce0e86c08f694a1235f689a3c891
Jean-Francois Chevrette <jfchevrette@gmail.com>
2019-06-13 08:38:22 -0400

    retag image before sending to quay.io

--- hack/build_deploy.sh
+++ hack/build_deploy.sh
@@ -12,5 +12,6 @@ docker build -t $IMAGE:$IMAGE_TAG .
 # Push image to quay.io
 if [[ -n "$QUAY_USER" && -n "$QUAY_TOKEN" ]]; then
   docker --config="$PWD/.docker" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
+  docker tag $IMAGE:$IMAGE_TAG quay.io/$IMAGE:$IMAGE_TAG
   docker --config="$PWD/.docker" push quay.io/$IMAGE:$IMAGE_TAG
 fi
